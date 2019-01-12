f6069c9873536cf753c0607cc48af406a4093b53
Anders Jakobsen <andersjkbsn@gmail.com>
2019-01-12 11:46:01 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 
 import numpy as np
-from random import choice
-
 from loguru import logger
 
 from load_data import load_fold
+
+# n20~1.12
 
 n_latent_factors = 20
 learning_rate = 0.001
@@ -32,14 +32,6 @@ def get_movies(from_set):
     return movies
 
 
-def get_random_pair(training_set):
-    """ Get a random movie-user pair with the associated rating """
-    user = choice(list(training_set.keys()))
-    movie = choice(list(training_set[user].keys()))
-
-    return user, movie, training_set[user][movie]
-
-
 def get_singular_vectors(n_movies, n_users):
     """ Return the left and right singular vectors """
     # Initialize singular value vectors
@@ -52,36 +44,34 @@ def get_singular_vectors(n_movies, n_users):
     # m = np.matmul(A, B)
 
 
-def run_sgd():
-    pass
-
-
 def calculate_rmse(on_set, A, B):
     # Compute the rating matrix R
     R = np.matmul(A, B)
 
-    # Calculate number of test instances
-    n_test_instances = 0
+    n_instances = 0
     sum_squared_errors = 0
     for user, movie_rating in on_set.items():
-        n_test_instances += len(movie_rating.keys())
+        n_instances += len(movie_rating.keys())
 
         for movie, rating in movie_rating.items():
             sum_squared_errors += pow(R[movie][user] - rating, 2)
 
-    return np.sqrt(sum_squared_errors / n_test_instances)
+    return np.sqrt(sum_squared_errors / n_instances)
 
 
 def run():
     train, test = load_fold(1)
 
-    # Retrieve the singular vectors
-    # A can be interpreted as movie factors
-    # B can be interpreted as user factors
-    A, B = get_singular_vectors(1700, 2000)
+    # Construct the singular vectors
+    movies = get_movies(train)
+    A, B = get_singular_vectors(max(movies) + 1, max(train.keys()) + 1)
 
+    # Training instances are represented as a list of tripes
     triples = get_triples(train)
+
     for epoch in range(max_epochs):
+        # At the start of every epoch, we shuffle the dataset
+        # Shuffling may not be strictly necessary, but is an attempt to avoid overfitting
         random.shuffle(triples)
 
         # Calculate RMSE for training set
@@ -104,6 +94,8 @@ def run():
                     t_sum += A[movie][i] * B[i][user]
 
                 gradient = (rating - t_sum) * A[movie][k]
+
+                # Update the kth factor with respect to the gradient and learning rate
                 A[movie][k] += learning_rate * (gradient - regularizer * B[k][user])
 
 
